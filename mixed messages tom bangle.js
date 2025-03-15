f621003535272395af25a19d113f3b68412b6145
Tom Bangle <tombangle@gmail.com>
2025-03-14 19:05:38 -0700

    First Change

--- mixed messages tom bangle.js
+++ mixed messages tom bangle.js
@@ -1,42 +1,9 @@
-function generateRandomNumber(num) {
-    // Gets # from 0 -> num - 1
-    return Math.floor(Math.random() * num)
+const generateRandomNumber = () => Math.floor(Math.random() * 10) + 1;
+  
+  const favoriteMotivation = {
+    mood: ['happy', 'sad', 'angry', 'excited', 'calm', 'anxious', 'grateful', 'bored', 'hopeful', 'lonely'],
+    motivation: ['you will never have this day again, so make it count!', 'wake up and be awesome.', 'stop waiting and start doing.', 'have faith, your best days are on their way.', 'overthinking is the quickest way to ruin something good.', 'skill is only developed by hours and hours of work.', 'asking for help reveals wisdom, not weakness.', 'hold the vision, trust the process', 'act without expectation', 'listen to many, speak to few.'],
   }
   
-  const collectiveWisdom = {
-    signInfo: ['star', 'moon', 'sun', 'comet'],
-    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
-    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
-  }
-  
-  // Store the 'wisdom' in an array
-  let personalWisdom = []
-  
-  // Iterate over the object
-  for(let prop in collectiveWisdom) {
-    let optionIdx = generateRandomNumber(collectiveWisdom[prop].length)
-  
-    // use the object's properties to customize the message being added to personalWisdom  
-    switch(prop) {
-      case 'signInfo':
-        personalWisdom.push(`Your sign right now is a "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'fortuneOutput':
-        personalWisdom.push(`You are having: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'advice':
-        personalWisdom.push(`You should: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      default:
-        personalWisdom.push('There is not enough info.')
-    }
-  }
-  
-  function formatWisdom(wisdom) {
-    // Add some ASCII here?
-    // Add in more symbols to the array? like emojies or what not?
-    const formatted = personalWisdom.join('\n')
-    console.log(formatted)
-  }
-  
-  formatWisdom(personalWisdom);
+  // Store the 'motivation' as an array
+  let personalMotivation = []
