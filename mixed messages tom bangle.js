f777f8bd6d64cd7c188253436d6de188ef7520a4
Tom Bangle <tombangle@gmail.com>
2025-03-14 19:08:10 -0700

    Second Change

--- mixed messages tom bangle.js
+++ mixed messages tom bangle.js
@@ -1,9 +1,20 @@
 const generateRandomNumber = () => Math.floor(Math.random() * 10) + 1;
   
   const favoriteMotivation = {
-    mood: ['happy', 'sad', 'angry', 'excited', 'calm', 'anxious', 'grateful', 'bored', 'hopeful', 'lonely'],
-    motivation: ['you will never have this day again, so make it count!', 'wake up and be awesome.', 'stop waiting and start doing.', 'have faith, your best days are on their way.', 'overthinking is the quickest way to ruin something good.', 'skill is only developed by hours and hours of work.', 'asking for help reveals wisdom, not weakness.', 'hold the vision, trust the process', 'act without expectation', 'listen to many, speak to few.'],
-  }
+   mood: ['happy', 'sad', 'angry', 'excited', 'calm', 'anxious', 'grateful', 'bored', 'hopeful', 'lonely'],
+   motivation: [
+      'You will never have this day again, so make it count!', 
+      'Wake up and be awesome.', 
+      'Stop waiting and start doing.', 
+      'Have faith, your best days are on their way.', 
+      'Overthinking is the quickest way to ruin something good.', 
+      'Skill is only developed by hours and hours of work.', 
+      'Asking for help reveals wisdom, not weakness.', 
+      'Hold the vision, trust the process.', 
+      'Act without expectation.', 
+      'Listen to many, speak to few.'
+    ],
+  };
   
   // Store the 'motivation' as an array
   let personalMotivation = []
